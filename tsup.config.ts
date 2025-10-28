3aca87ce9f67269b7ebdde07387e727e61030c18
maanimis <maanimis.dev@gmail.com>
2025-10-28 11:35:53 +0330

    chore:change package name

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: ["src/index.ts"],
   format: ["cjs", "esm", "iife"],
-  globalName: "YoctoQueue2",
+  globalName: "yoctoqueue2",
   splitting: false,
   sourcemap: true,
   clean: true,
